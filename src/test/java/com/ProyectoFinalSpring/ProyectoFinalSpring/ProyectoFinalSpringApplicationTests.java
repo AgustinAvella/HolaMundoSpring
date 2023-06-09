275b6cb27112b086a2ed5287b450bbde649ffa1c
AgustinAvella <aavella@americavirtualsa.com>
2023-06-09 10:27:01 -0300

    first commit

---
+++ src/test/java/com/ProyectoFinalSpring/ProyectoFinalSpring/ProyectoFinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinalSpring.ProyectoFinalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
